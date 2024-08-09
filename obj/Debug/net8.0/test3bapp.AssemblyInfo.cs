964c3a83145fdf6eb40e17a2e8fd09a9578f757d
Neil Morrissey <neil@neilmorrissey.net>
2024-08-08 17:55:05 -0700

    Updated Index page

--- obj/Debug/net8.0/test3bapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/test3bapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test3bapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2766b246964f109813623285f7ed13ab6ede3e4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("test3bapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test3bapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
